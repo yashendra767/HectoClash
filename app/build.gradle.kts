f3305be39a5d4c49ab5a79523e6a3e0d84674654
yashe <yashendraawasthi@gmail.com>
2025-04-05 04:17:45 +0530

    fragment done push yash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.hectoclash"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -45,7 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    //circle image
     implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
--- app/src/main/java/com/example/hectoclash/MainActivity.kt
+++ app/src/main/java/com/example/hectoclash/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.hectoclash
 
+import android.R.attr.fragment
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val bottomView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        replaceTheFragment(home())
+        bottomView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.item1 -> replaceTheFragment(home())
+                R.id.item2 -> replaceTheFragment(Leaderboard())
+                R.id.item3 -> replaceTheFragment(More())
+            }
+            true
+        }
+
+    }
+    fun replaceTheFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+
+    }
     }
 }
